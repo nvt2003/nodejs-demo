46ea1a10568669560a680a7a1cb82071418dff73
Thu <thunvhe176252@fpt.edu.vn>
2026-09-07 14:24:48 +0700

    remove unuse funtion

--- src/backend/utils/auth.js
+++ src/backend/utils/auth.js
@@ -1,23 +1,6 @@
 import { getSession } from './session.js';
 import sendJSON from './sendJson.js';
 
-// Phân quyền theo Role
-// trả về thông tin session
-export function authorize(req, res, allowedRoles = []) {
-  const session = getSession(req);
-  //kiểm tra đăng nhập
-  if (!session) {
-    sendJSON(res, 401, { message: 'Chưa đăng nhập hoặc phiên làm việc hết hạn' });
-    return null;
-  }
-  //kiểm tra quyền
-  if (allowedRoles.length > 0 && !allowedRoles.includes(session.role)) {
-    sendJSON(res, 403, { message: 'Bạn không có quyền truy cập chức năng này' });
-    return null;
-  }
-
-  return session;
-}
 //Kiểm tra quyền người dùng
 //Nếu đủ quyền thì tiếp tục request với thông tin phiên đăng nhập
 export function checkRole(allowedRoles = [], handler) {
